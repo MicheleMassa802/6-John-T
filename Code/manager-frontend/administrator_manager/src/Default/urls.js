1d87bf26d9df702c7030705c155366548e39bafb
Michele Massa <michele.massa@mail.utoronto.ca>
2023-08-20 16:01:45 -0400

    backend url set

--- Code/manager-frontend/administrator_manager/src/Default/urls.js
+++ Code/manager-frontend/administrator_manager/src/Default/urls.js
@@ -1,5 +1,6 @@
 //taken from user frontend side
-import isProd from "./isProd";
+// import isProd from "./isProd";
 
-export const BACKEND_URL = isProd() ? 'https://6-john-t-production-f786.up.railway.app' : 'https://6-john-t-production.up.railway.app';
+// export const BACKEND_URL = isProd() ? 'https://6-john-t-production-f786.up.railway.app' : 'https://6-john-t-production.up.railway.app';
+export const BACKEND_URL = 'https://6-john-t-production-9cc8.up.railway.app';
 //export const BACKEND_URL = 'http://127.0.0.1:8000';
--- Code/user-frontend/src/default_values/constants.js
+++ Code/user-frontend/src/default_values/constants.js
@@ -1,5 +1,6 @@
 import isProd from "../utils/isProd";
 
-export const BACKEND_URL = isProd() ? 'https://6-john-t-production-f786.up.railway.app' : 'https://6-john-t-production.up.railway.app';
+// export const BACKEND_URL = isProd() ? 'https://6-john-t-production-f786.up.railway.app' : 'https://6-john-t-production.up.railway.app';
+export const BACKEND_URL = 'https://6-john-t-production-9cc8.up.railway.app';
 
 export const BACKEND_PATH_FOR_BENCH_DETAILS = '/benches/get_user_bench/';
